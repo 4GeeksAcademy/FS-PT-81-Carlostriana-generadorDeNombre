262e005ce43fe0d11e78edd37501ddf1f099644f
Carlos03tb <carlostb0318@gmail.com>
2024-10-14 13:12:11 +0000

    corregido

--- src/app.js
+++ src/app.js
@@ -11,15 +11,8 @@ window.onload = function() {
   let noun = ["jogger", "racoon"];
   let action = ["ran away", "jumped off the cliff"];
 
-  let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
-  let randomAdj = adj[Math.floor(Math.random() * adj.length)];
-  let randomNoun = noun[Math.floor(Math.random() * noun.length)];
-  let randomAction = action[Math.floor(Math.random() * action.length)];
-
-  let generator = `${randomPronoun} ${randomAdj.charAt(0).toLowerCase() +
-    randomAdj.slice(1)} ${randomNoun.charAt(0).toLowerCase() +
-    randomNoun.slice(1)} ${randomAction.charAt(0).toLowerCase() +
-    randomAction.slice(1)}`;
+  const getRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+  const generator = `${getRandom(pronoun)} ${getRandom(adj)} ${getRandom(noun)} ${getRandom(action)}`;
 
   console.log(generator);
 };
